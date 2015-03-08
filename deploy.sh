4dcc768fea5f583acf969a1f09dd60fee1cb8e33
Mario Idival <marioidival@gmail.com>
2015-03-08 08:44:48 -0300

    test <#

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.name "Blog-Bot"
     fi
     # using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://git@github.com/$TARGET_REPO built_website > /dev/null
+    git clone --quiet --branch=$BRANCH https://$GH_TOKEN@github.com/$TARGET_REPO built_website > /dev/null
     # go into directory and copy data we're interested in to that directory
     cd built_website
     # limpa branch master para nao ficar arquivos lixo, como arquivos excluidos e/ou renomeados
